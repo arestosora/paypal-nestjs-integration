2bab47ff416d3305ec99337ccdd2571d0c8bf6b5
A. Mestra <adonymestra2@gmail.com>
2024-10-07 23:06:56 -0500

    chore: cleaning code

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,43 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as session from 'express-session';
+import * as passport from 'passport';
+import { NestFactory, NestApplication } from '@nestjs/core';
+import { AppModule } from './app.module';
 import { TypeormStore } from 'connect-typeorm';
 import { Session } from './modules/database/schemas/Session';
 import { DataSource } from 'typeorm';
-import * as passport from 'passport';
+import config from './config';
 
-async function main() {
-  const app = await NestFactory.create(AppModule);
+async function configureSession(app: NestApplication): Promise<void> {
   const sessionRepository = app.get(DataSource).getRepository(Session);
-  app.setGlobalPrefix('v1/api');
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 24 * 7
-    },
-    store: new TypeormStore().connect(sessionRepository)
-  }))
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: config.app.cookieMaxAge,
+      },
+      store: new TypeormStore().connect(sessionRepository),
+    }),
+  );
+}
+
+async function configurePassport(app: NestApplication): Promise<void> {
   app.use(passport.initialize());
   app.use(passport.session());
-  app.enableCors()
+}
+
+async function configureApp(app: NestApplication): Promise<void> {
+  app.setGlobalPrefix(config.app.globalPrefix);
+  app.enableCors();
+}
+
+async function main(): Promise<void> {
+  const app: NestApplication = await NestFactory.create(AppModule);
+  await configureSession(app);
+  await configurePassport(app);
+  configureApp(app);
   await app.listen(process.env.PORT || 3000);
 }
+
 main();
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { Services } from 'src/common/constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,7 +6,6 @@ import { Users } from '../database/schemas/Users';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
-  controllers: [UsersController],
   providers: [{
     provide: Services.USER,
     useClass: UsersService
