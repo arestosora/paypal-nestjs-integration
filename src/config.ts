30d677d44715c4c854e2540871a492030499b79d
A. Mestra <adonymestra2@gmail.com>
2024-11-16 20:36:28 -0500

    update packages

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { Time } from "@sapphire/cron";
 
 export default {
     app: {
-        globalPrefix: 'dev/api/v1',
+        globalPrefix: 'api/v1',
         cookieMaxAge: Time.Day * 2
     },
 }
--- src/modules/auth/interfaces/auth.ts
+++ src/modules/auth/interfaces/auth.ts
@@ -1,5 +1,6 @@
 import { UserDetails } from "src/common/types";
 import { Users } from "src/modules/database/schemas/Users";
+
 export interface IAuthService {
     validate(details: UserDetails): Promise<Users>;
 }
